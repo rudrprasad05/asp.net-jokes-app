846339c72962e34cc3f5fa7763a9173c56f72359
rudr <rudr.prasad@inov8.co.nz>
2024-12-04 12:07:44 +1200

    .env

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+Dotenv.Config();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -16,7 +17,6 @@ builder.Services.AddCors(options =>
     });
 });
 
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline. test
@@ -26,11 +26,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
-app.UseCors("AllowLocalhost");
-
-app.UseAuthorization();
+app
+.UseHttpsRedirection()
+.UseCors("AllowLocalhost")
+.UseAuthorization();
 
 app.MapControllers();
 
